fb3d2abb9cf68f4d4893607dd8a9f082cefdc3a3
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-01-29 15:00:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a stack/sort-a-stack.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.util.Scanner;
+import java.util.Stack;
+
+class SortedStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            Stack<Integer> s = new Stack<>();
+            int n = sc.nextInt();
+            while (n-- > 0) s.push(sc.nextInt());
+            GfG g = new GfG();
+            Stack<Integer> a = g.sort(s);
+            while (!a.empty()) {
+                System.out.print(a.peek() + " ");
+                a.pop();
+            }
+            System.out.println();
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+class GfG {
+    public Stack<Integer> sort(Stack<Integer> s) {
+        // add code here.
+        ArrayList<Integer> arr=new ArrayList<>();
+        while(!s.isEmpty())
+        {
+            arr.add(s.pop());
+        }
+        Collections.sort(arr);
+        for(int i=0;i<arr.size();i++)
+        {
+            s.add(arr.get(i));
+        }
+        return s;
+    }
+}
