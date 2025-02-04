b9c49faa3abb454711ad55e840fb5bef54419228
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-02-04 20:30:22 +0530

    Time: 1 ms, Memory: 42.4 MB - LeetHub

---
+++ 3458-463-520-detect-capital/3458-463-520-detect-capital.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.length()==0) return false;
+        int count=0;
+        for(int i=0;i<word.length();i++)
+        {
+            if(Character.isUpperCase(word.charAt(i)))
+            {
+                count++;
+            }
+        }
+        if(count==0) return true;
+        if(count==1 && Character.isUpperCase(word.charAt(0))) return true;
+        if(count==word.length()) return true;
+        return false;
+    }
+}
