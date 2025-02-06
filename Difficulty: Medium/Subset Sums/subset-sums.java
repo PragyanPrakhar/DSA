70bb90586d1cea84811d01a6bfe15a32f641f8c2
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-02-06 17:32:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sums/subset-sums.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            ArrayList<Integer> result = new Solution().subsetSums(arr);
+            Collections.sort(result);
+            // Printing the result in space-separated format
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0) System.out.print(" ");
+                System.out.print(result.get(i));
+            }
+            System.out.println();    // Print new line after each test case
+            System.out.println("~"); // Print new line after each test case
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java//User function Template for Java
+class Solution {
+    public ArrayList<Integer> subsetSums(int[] arr) {
+        // code here
+        ArrayList<Integer> ar=new ArrayList<>();
+        finding(arr,0,0,ar);
+        Collections.sort(ar);
+        return ar;
+    }
+    private void finding(int[] arr,int sum,int index,ArrayList<Integer> ar)
+    {
+        if(index==arr.length)
+        {
+            ar.add(sum);
+            return;
+        }
+        finding(arr,sum+arr[index],index+1,ar);
+        finding(arr,sum,index+1,ar);
+    }
+}
