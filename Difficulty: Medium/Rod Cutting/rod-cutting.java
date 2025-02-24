437e1c507d41fcb19a5288880fbcf499e09fb6c6
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-02-24 22:26:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rod Cutting/rod-cutting.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class RodCutting {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String s[] = in.readLine().trim().split(" ");
+            int[] arr = new int[s.length];
+            for (int i = 0; i < s.length; i++) arr[i] = Integer.parseInt(s[i]);
+
+            Solution ob = new Solution();
+            out.println(ob.cutRod(arr));
+
+            out.println("~");
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    public int cutRod(int[] price) {
+        // Average
+        int n=price.length;
+        int []dp=new int[n+1];
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                dp[i]=Math.max(dp[i],price[j]+dp[i-j-1]);
+            }
+        }
+        return dp[n];
+    }
+}
