8b8cbc025d993ff91f6145a47351871eb995cd49
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-02-25 10:04:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.java
+++ Difficulty: Medium/Rod Cutting/rod-cutting.java
@@ -27,14 +27,19 @@ class RodCutting {
 
 class Solution {
     public int cutRod(int[] price) {
-        // Average
         int n=price.length;
-        int []dp=new int[n+1];
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<i;j++){
-                dp[i]=Math.max(dp[i],price[j]+dp[i-j-1]);
-            }
-        }
-        return dp[n];
+        int dp[][]=new int[n][n+1];
+        for(int ar[]:dp) Arrays.fill(ar,-1);
+        return solve(price,n,n-1,0,dp);
+    }
+    private int solve(int price[],int n,int ind,int count,int dp[][]){
+        if(ind<0) return 0;
+        if(count==n) return 0;
+        if(dp[ind][count]!=-1) return dp[ind][count];
+        int not_take=solve(price,n,ind-1,count,dp);
+        int take=0;
+        int rodLength=ind+1;
+        if(count+rodLength <= n) take=price[ind]+solve(price,n,ind,count+rodLength,dp);
+        return dp[ind][count]=Math.max(not_take,take);
     }
 }
