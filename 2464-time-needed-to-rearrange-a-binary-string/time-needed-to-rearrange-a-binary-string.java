505df94fd7f810a5618609a5a8a9106ab9cc950a
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-03-21 11:38:23 +0530

    Time: 173 ms (73.45%) | Memory: 41.6 MB (88.32%) - LeetSync

--- 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.java
+++ 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.java
@@ -1,21 +1,29 @@
 class Solution {
     public int secondsToRemoveOccurrences(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        int operations = 0;
-        boolean swapped = true;
-
-        while (swapped) {
-            swapped = false;
-            for (int i = 0; i < sb.length() - 1; i++) {
-                if (sb.charAt(i) == '0' && sb.charAt(i + 1) == '1') {
-                    sb.setCharAt(i, '1');
-                    sb.setCharAt(i + 1, '0');
-                    i++; // Skip next index to avoid double swap
-                    swapped = true;
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        int prevIndex = -1;
+        char prevChar = '$';
+        int numberOfOperations = 0;
+        boolean p = true;
+        while (p) {
+            boolean q = false;
+            for (int i = 0; i < s.length(); i++) {
+                if (sb.charAt(i) == '1' && prevChar == '0') {
+                    sb.setCharAt(i - 1, '1');
+                    sb.setCharAt(i, '0');
+                    i++;
+                    q = true;
                 }
+                if(i>sb.length()-1) break;
+                prevChar = sb.charAt(i);
             }
-            if (swapped) operations++;
+            if (q)
+                numberOfOperations++;
+            if (!q)
+                p = false;
+            prevChar = '$';
         }
-        return operations;
+        return numberOfOperations;
     }
 }
