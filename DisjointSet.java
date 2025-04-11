5e8099f1a8ab7650f745c8a21534fc0ce6657d64
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-04-11 13:12:55 +0530

    Disjoint set

---
+++ DisjointSet.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisjointSet {
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            parent.add(i);
+            rank.add(0);
+        }
+    }
+
+    public int findUltimateParent(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ultimateParent = findUltimateParent(parent.get(node));
+        parent.set(node, ultimateParent);
+        return ultimateParent;
+    }
+
+    public void unionByRank(int u, int v) {
+        int ultimateParentU = findUltimateParent(u);
+        int ultimateParentV = findUltimateParent(v);
+        if (ultimateParentU == ultimateParentV) {
+            return;
+        }
+        if (rank.get(ultimateParentU) < rank.get(ultimateParentV)) {
+            parent.set(ultimateParentU, ultimateParentV);
+        } else if (rank.get(ultimateParentU) > rank.get(ultimateParentV)) {
+            parent.set(ultimateParentV, ultimateParentU);
+        } else {
+            parent.set(ultimateParentV, ultimateParentU);
+            rank.set(ultimateParentU, rank.get(ultimateParentU) + 1);
+        }
+    }
+
+    public void unionBySize(int u, int v) {
+        int ultimateParentU = findUltimateParent(u);
+        int ultimateParentV = findUltimateParent(v);
+        if (ultimateParentU == ultimateParentV) {
+            return;
+        }
+        if (size.get(ultimateParentU) < size.get(ultimateParentV)) {
+            parent.set(ultimateParentU, ultimateParentV);
+            size.set(ultimateParentV, size.get(ultimateParentV) + size.get(ultimateParentU));
+        } else {
+            parent.set(ultimateParentV, ultimateParentU);
+            size.set(ultimateParentU, size.get(ultimateParentU) + size.get(ultimateParentV));
+        }
+
+    }
+
+    public static void main(String[] args) {
+        DisjointSet ds = new DisjointSet(10);
+        ds.unionByRank(1, 2);
+        ds.unionByRank(2, 3);
+        ds.unionByRank(4, 5);
+        ds.unionByRank(6, 7);
+        ds.unionByRank(5, 6);
+        ds.unionByRank(3, 4);
+        ds.unionByRank(8, 9);
+        ds.unionByRank(7, 8);
+        ds.unionByRank(1, 9);
+        System.out.println(ds.findUltimateParent(1)); // Output: 9
+        System.out.println(ds.findUltimateParent(2)); // Output: 9
+
+        // now union by size
+        ds.unionBySize(1, 2);
+        ds.unionBySize(3, 4);
+        ds.unionBySize(5, 6);
+        ds.unionBySize(7, 8);
+
+        System.out.println(ds.findUltimateParent(1)); // Output: 9
+    }
+}
+
+// class Main {
+
+// }
