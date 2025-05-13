d15c3445ef8b6e83aa36acac586ce9331bee84da
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-05-13 11:25:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (11.04%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -4,14 +4,19 @@ class Solution {
         for(int ar[]:dp){
             Arrays.fill(ar,-1);
         }
-        return solve(m,n,0,0,dp);
+        int i=0;
+        int j=0;
+        return solve(i,j,m,n,dp);
     }
-    private int solve(int m,int n,int i,int j,int dp[][]){
+    private int solve(int i,int j,int m,int n,int dp[][]){
+        if(i<0 || j<0 || i>=m || j>=n){
+            return 0;
+        }
         if(i==m-1 && j==n-1) return 1;
-        if(i<0 || i>=m || j<0 || j>=n) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int right=solve(m,n,i,j+1,dp);
-        int down=solve(m,n,i+1,j,dp);
-        return dp[i][j]=right+down;
+        int down=solve(i+1,j,m,n,dp);
+        int right=solve(i,j+1,m,n,dp);
+        return dp[i][j]=down+right;
+
     }
 }
