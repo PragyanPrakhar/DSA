1c7cbb7e9acaccd7a7956dfe36ea871d0eff6d21
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-05-29 16:31:12 +0530

    Time: 10 ms (96.15%) | Memory: 48.9 MB (43.01%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,19 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        int ans[]=new int[k];
-        int ind=0;
-        for(int i:nums)
-        {
-            hm.put(i,hm.getOrDefault(i,0)+1);
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<Map.Entry<Integer,Integer>> pq=new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
-        pq.addAll(hm.entrySet());
-        for(int i=0;i<k;i++)
-        {
-            ans[ind++]=pq.poll().getKey();
+
+        // Create an array of buckets where index = frequency
+        List<Integer>[] buckets = new List[nums.length + 1];
+        for (int key : freqMap.keySet()) {
+            int freq = freqMap.get(key);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(key);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        // Traverse buckets from high freq to low
+        for (int i = buckets.length - 1; i >= 0 && result.size() < k; i--) {
+            if (buckets[i] != null) {
+                result.addAll(buckets[i]);
+            }
+        }
+
+        // Convert result to array
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = result.get(i);
         }
         return ans;
-        
     }
 }
