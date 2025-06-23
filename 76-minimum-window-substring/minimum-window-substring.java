53de0018b542cff581f8bddb2b8768dc28af8520
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-06-24 00:49:57 +0530

    Time: 21 ms (36.78%) | Memory: 45.4 MB (36.06%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,47 +1,42 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) return "";
-
-        Map<Character, Integer> tMap = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
-        }
-
-        Map<Character, Integer> windowMap = new HashMap<>();
-        int start = 0, end = 0;
+        int end = 0;
+        int start = 0;
         int minLen = Integer.MAX_VALUE;
-        int minStart = 0;
-        int formed = 0; // Number of characters meeting the required count
-
+        if(t.length()>s.length()) return "";
+        HashMap<Character, Integer> tMap = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        String ans = "";
+        HashMap<Character, Integer> windowMap = new HashMap<>();
+        int minStart=0;
+        int formed = 0;
         while (end < s.length()) {
             char endChar = s.charAt(end);
             if (tMap.containsKey(endChar)) {
                 windowMap.put(endChar, windowMap.getOrDefault(endChar, 0) + 1);
-                if (windowMap.get(endChar).intValue() == tMap.get(endChar).intValue()) {
+                if (windowMap.get(endChar).equals(tMap.get(endChar))) {
                     formed++;
                 }
             }
-
             while (formed == tMap.size()) {
-                // Update min window
                 if (end - start + 1 < minLen) {
-                    minLen = end - start + 1;
-                    minStart = start;
+                    minLen=end-start+1;
+                    minStart=start;
                 }
-
-                char startChar = s.charAt(start);
-                if (tMap.containsKey(startChar)) {
-                    windowMap.put(startChar, windowMap.get(startChar) - 1);
-                    if (windowMap.get(startChar) < tMap.get(startChar)) {
+                char startChar=s.charAt(start);
+                if(tMap.containsKey(startChar)){
+                    windowMap.put(startChar,windowMap.get(startChar)-1);
+                    if(windowMap.get(startChar)<tMap.get(startChar)){
                         formed--;
                     }
                 }
                 start++;
             }
-
             end++;
         }
+        return minLen==Integer.MAX_VALUE ? "" : s.substring(minStart,minStart+minLen);
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
     }
 }
