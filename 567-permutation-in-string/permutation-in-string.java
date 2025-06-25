976c33fa3890e8f1ce29bcc4bae8fe1e6ef93377
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-06-26 00:10:27 +0530

    Time: 6 ms (88.00%) | Memory: 42.5 MB (89.34%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,27 +1,25 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length())
-            return false;
-        HashMap<Character, Integer> hm1 = new HashMap<>();
-        HashMap<Character, Integer> hm2 = new HashMap<>();
-        for (char ch : s1.toCharArray()) {
-            hm1.put(ch, hm1.getOrDefault(ch, 0) + 1);
-            hm2.put(ch, hm2.getOrDefault(ch, 0) + 1);
+        if(s1.length()>s2.length()) return false;
+        int[] freqS1=new int[26];
+        int[] freqS2=new int[26];
+        for(char ch:s1.toCharArray()){
+            freqS1[ch-'a']++;
         }
-        int wS = s1.length();
-        for (int i = 0; i <= s2.length() - wS; i++) {
-            for (int j = i; j < i + wS; j++) {
-                if (hm1.containsKey(s2.charAt(j))) {
-                    hm1.put(s2.charAt(j), hm1.get(s2.charAt(j)) - 1);
-                    if (hm1.get(s2.charAt(j)) == 0)
-                        hm1.remove(s2.charAt(j));
-                    if (hm1.size() == 0)
-                        return true;
-                }
-            }
-            hm1 = new HashMap<>(hm2);
+        for(int i=0;i<s1.length();i++){
+            freqS2[s2.charAt(i)-'a']++;
         }
-        return false;
-
+        for(int i=s1.length();i<s2.length();i++){
+            if(isEqual(freqS1,freqS2)) return true; 
+            freqS2[s2.charAt(i)-'a']++;
+            freqS2[s2.charAt(i-s1.length())-'a']--;
+        }
+        return isEqual(freqS1,freqS2);
+    }
+    private boolean isEqual(int[] freqS1,int[] freqS2){
+        for(int i=0;i<26;i++){
+            if(freqS1[i]!=freqS2[i]) return false;
+        }
+        return true;
     }
 }
