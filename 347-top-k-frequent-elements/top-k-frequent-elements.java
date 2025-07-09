9024e835d9da159132d2f17e065b2011c78edc66
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-07-09 20:44:17 +0530

    Time: 11 ms (92.10%) | Memory: 45.3 MB (98.75%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,33 +1,27 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        for (int num : nums) {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int num:nums){
+            hm.put(num,hm.getOrDefault(num,0)+1);
         }
-
-        // Create an array of buckets where index = frequency
-        List<Integer>[] buckets = new List[nums.length + 1];
-        for (int key : freqMap.keySet()) {
-            int freq = freqMap.get(key);
-            if (buckets[freq] == null) {
-                buckets[freq] = new ArrayList<>();
+        List<Integer>[] buckets=new List[nums.length+1];
+        for(int num:hm.keySet()){
+            int freq=hm.get(num);
+            if(buckets[freq]==null){
+                buckets[freq]=new ArrayList<>();
             }
-            buckets[freq].add(key);
+            buckets[freq].add(num);
         }
-
-        List<Integer> result = new ArrayList<>();
-        // Traverse buckets from high freq to low
-        for (int i = buckets.length - 1; i >= 0 && result.size() < k; i--) {
-            if (buckets[i] != null) {
-                result.addAll(buckets[i]);
+        ArrayList<Integer> ans=new ArrayList<>();
+        for(int i=buckets.length-1;i>=0 && ans.size()<k;i--){
+            if(buckets[i]!=null){
+                ans.addAll(buckets[i]);
             }
         }
-
-        // Convert result to array
-        int[] ans = new int[k];
-        for (int i = 0; i < k; i++) {
-            ans[i] = result.get(i);
+        int res[]=new int[k];
+        for(int i=0;i<k;i++){
+            res[i]=ans.get(i);
         }
-        return ans;
+        return res;
     }
 }
