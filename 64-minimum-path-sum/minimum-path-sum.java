c7cc5e673adfbdcc964971bd1ee77bbb81faf3ac
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-07-10 23:26:43 +0530

    Time: 1 ms (99.67%) | Memory: 47.6 MB (68.63%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        // Trying out all the paths --> Recursive Approach
+        int m = grid.length;
+        int n = grid[0].length;
+        int dp[][] = new int[m][n];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
+        dp[0][0] = 1;
+        return finding(m - 1, n - 1, grid, dp);
+    }
+
+    private int finding(int m, int n, int[][] grid,int[][] dp) {
+        if (m < 0 || n < 0)
+            return Integer.MAX_VALUE;
+        if (m == 0 && n == 0)
+            return grid[m][n];
+        if(dp[m][n]!=-1) return dp[m][n];
+        int up = finding(m - 1, n, grid,dp);
+        int left = finding(m, n - 1, grid,dp);
+        return dp[m][n]=(grid[m][n] + Math.min(up, left));
+    }
+}
