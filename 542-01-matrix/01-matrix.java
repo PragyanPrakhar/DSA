740683eb99a083a9df45aa542029f035777689ff
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-07-15 00:48:11 +0530

    Time: 15 ms (35.17%) | Memory: 47.1 MB (73.39%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,53 +1,48 @@
-class Node {
+class Node{
     int first;
     int second;
     int third;
-
-    Node(int first, int second, int third) {
-        this.first = first;
-        this.second = second;
-        this.third = third;
+    Node(int first,int second,int third){
+        this.first=first;
+        this.second=second;
+        this.third=third;
     }
 }
-
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        Queue<Node> q = new LinkedList<Node>();
-        int row = mat.length;
-        int col = mat[0].length;
-        int vis[][] = new int[row][col];
-        int dis[][] = new int[row][col];
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (mat[i][j] == 0) {
-                    q.add(new Node(i, j, 0));
-                    vis[i][j] = 1;
-                } else {
-                    vis[i][j] = 0;
+        int rows=mat.length;
+        int cols=mat[0].length;
+        int vis[][]=new int[rows][cols];
+        Queue<Node> q=new LinkedList<>();
+        int dist[][]=new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(mat[i][j]==0){
+                    q.add(new Node(i,j,0));
+                    vis[i][j]=1;
+                }
+                else{
+                    vis[i][j]=0;
                 }
             }
         }
-        while (!q.isEmpty()) {
-            int n = q.peek().first;
-            int m = q.peek().second;
-            int steps = q.peek().third;
+        while(!q.isEmpty()){
+            int row=q.peek().first;
+            int col=q.peek().second;
+            int steps=q.peek().third;
             q.remove();
-
-            dis[n][m] = steps;
-            for (int i = -1; i <= 1; i++) {
-                for (int j = -1; j <= 1; j++) {
-                    int delRow = n + i;
-                    int delCol = m + j;
-                    if ((i == 0 || j == 0) && (delRow >= 0) && (delRow < row) && (delCol >= 0) && (delCol < col)
-                            && (vis[delRow][delCol] == 0)) {
-                        vis[delRow][delCol] = 1;
-                        q.add(new Node(delRow, delCol, steps + 1));
+            dist[row][col]=steps;
+            for(int i=-1;i<=1;i++){
+                for(int j=-1;j<=1;j++){
+                    int delRow=row+i;
+                    int delCol=col+j;
+                    if((i==0 || j==0)&&(delRow>=0) && (delCol>=0) && (delRow<rows) && (delCol < cols) && (vis[delRow][delCol]==0)){
+                        q.add(new Node(delRow,delCol,steps+1));
+                        vis[delRow][delCol]=1;
                     }
                 }
             }
         }
-        return dis;
-
+        return dist;
     }
 }
