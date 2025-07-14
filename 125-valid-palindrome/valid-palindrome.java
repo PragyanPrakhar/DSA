008c8b31f50731ecf259bc19641911f431dc91b9
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-07-14 14:38:05 +0530

    Time: 4 ms (56.71%) | Memory: 43.4 MB (61.41%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,21 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase();
         StringBuilder sb=new StringBuilder();
+        s=s.toLowerCase();
         for(int i=0;i<s.length();i++){
-            if((s.charAt(i) >= 'a' && s.charAt(i) <= 'z') || (s.charAt(i) >= '0' && s.charAt(i) <= '9')){
+            if((s.charAt(i)>='a' && s.charAt(i)<='z') || (s.charAt(i)>='0' && s.charAt(i)<='9')){
                 sb.append(s.charAt(i));
             }
         }
-        return checkPallindrome(sb.toString());
-    }
-    private boolean checkPallindrome(String s){
         int i=0;
-        int j=s.length()-1;
+        int j=sb.length()-1;
         while(i<=j){
-            if(s.charAt(i)!=s.charAt(j)) return false;
-            i++;
-            j--;
+            if(sb.charAt(i)==sb.charAt(j)){
+                i++;
+                j--;
+            }
+            else return false;
         }
         return true;
     }
