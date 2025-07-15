4ce878847a1f07ba7199fb702b9af6695176ba91
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-07-16 00:18:46 +0530

    Time: 1 ms (46.23%) | Memory: 45 MB (50.35%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,29 +1,38 @@
+class Node{
+    int row;
+    int col;
+    Node(int row,int col){
+        this.row=row;
+        this.col=col;
+    }
+}
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int initialColor=image[sr][sc];
-        int row=image.length;
-        int col=image[0].length;
-        int ans[][]=new int[row][col];
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                ans[i][j]=image[i][j];
-            }
-        }
-        dfs(sr,sc,image,ans,color);
-        return ans;
-    }
-    private void dfs(int sr,int sc,int image[][],int ans[][],int color){
-        ans[sr][sc]=color;
-        int n=image.length;
-        int m=image[0].length;
-        for(int i=-1;i<=1;i++){
-            for(int j=-1;j<=1;j++){
-                int delRow=sr+i;
-                int delCol=sc+j;
-                if((i==0 || j==0) && delRow>=0 && delRow<n && delCol >=0 && delCol<m && image[delRow][delCol]==image[sr][sc] && ans[delRow][delCol]!=color){
-                    dfs(delRow,delCol,image,ans,color);
+        int rows=image.length;
+        int cols=image[0].length;
+        int vis[][]=new int[rows][cols];
+        int originalColor=image[sr][sc];
+        if(image[sr][sc]==color) return image;
+        vis[sr][sc]=1;
+        image[sr][sc]=color;
+        Queue<Node> q=new LinkedList<>();
+        q.add(new Node(sr,sc));
+        while(!q.isEmpty()){
+            int row=q.peek().row;
+            int col=q.peek().col;
+            q.remove();
+            for(int i=-1;i<=1;i++){
+                for(int j=-1;j<=1;j++){
+                    int delRow=row+i;
+                    int delCol=col+j;
+                    if((i==0 || j==0) && (i!=j) && delRow>=0 && delRow<rows && delCol>=0 && delCol<cols && vis[delRow][delCol]==0 && image[delRow][delCol]==originalColor){
+                        vis[delRow][delCol]=1;
+                        image[delRow][delCol]=color;
+                        q.add(new Node(delRow,delCol));
+                    }
                 }
             }
         }
-    } 
+        return image;
+    }
 }
