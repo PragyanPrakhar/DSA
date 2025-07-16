cc354d438274159dd914c5d8cc93505d95b6116c
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-07-17 01:06:15 +0530

    Time: 1 ms (61.90%) | Memory: 45.6 MB (12.03%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,35 +1,27 @@
-import java.util.*;
-
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int n = graph.length;
-        int[] colors = new int[n]; // 0 -> unvisited, 1 -> color1, -1 -> color2
-        Arrays.fill(colors, 0); 
-
-        for (int i = 0; i < n; i++) {
-            if (colors[i] == 0) { // Start BFS for each component
-                if (!bfs(graph, i, colors)) {
-                    return false;
-                }
+        int n=graph.length;
+        int m=graph[0].length;
+        int colors[]=new int[n];
+        for(int i=0;i<n;i++){
+            if(colors[i]==0){
+                if(!bfs(graph,i,colors)) return false;
             }
         }
         return true;
     }
-
-    private boolean bfs(int[][] graph, int start, int[] colors) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
-        colors[start] = 1; // Start with color 1
-
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            for (int neighbor : graph[node]) {
-                if (colors[neighbor] == 0) { // Unvisited, assign opposite color
-                    colors[neighbor] = -colors[node];
-                    queue.offer(neighbor);
-                } else if (colors[neighbor] == colors[node]) {
-                    return false; // Conflict found
+    private boolean bfs(int graph[][],int startNode,int colors[]){
+        Queue<Integer> q=new LinkedList<>();
+        colors[startNode]=1;
+        q.offer(startNode);
+        while(!q.isEmpty()){
+            int node=q.poll();
+            for(int ne:graph[node]){
+                if(colors[ne]==0){
+                    colors[ne]=-colors[node];
+                    q.offer(ne);
                 }
+                else if(colors[node]==colors[ne]) return false;
             }
         }
         return true;
