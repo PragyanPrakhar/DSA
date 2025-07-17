55b880809b8b74e149eefd9be37eb3ed5015e6c1
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-07-17 16:01:36 +0530

    Time: 634 ms (49.56%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,6 +1,3 @@
--- # Write your MySQL query statement below
--- (2,2),(4,7),(1,3)
-# Write your MySQL query statement below
 select 
     round(avg(order_date = customer_pref_delivery_date)*100,2) as immediate_percentage
 from 
