9d83996e400044824711fae9005df4b8b782e452
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-08-02 23:35:47 +0530

    Time: 3 ms (70.56%), Space: 47.5 MB (68.5%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,24 +1,33 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        // Trying out all the paths --> Recursive Approach
-        int m = grid.length;
-        int n = grid[0].length;
-        int dp[][] = new int[m][n];
-        for (int[] arr : dp) {
-            Arrays.fill(arr, -1);
+        int dp[][]=new int[grid.length][grid[0].length];
+        int rows=grid.length;
+        int cols=grid[0].length;
+        dp[0][0]=grid[0][0];
+        for(int i=1;i<cols;i++){
+            dp[0][i]=dp[0][i-1]+grid[0][i];
         }
-        dp[0][0] = 1;
-        return finding(m - 1, n - 1, grid, dp);
+        for(int j=1;j<rows;j++){
+            dp[j][0]=dp[j-1][0]+grid[j][0];
+        }
+        for(int i=1;i<rows;i++){
+            for(int j=1;j<cols;j++){
+                dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[rows-1][cols-1];
     }
-
-    private int finding(int m, int n, int[][] grid,int[][] dp) {
-        if (m < 0 || n < 0)
+    private int solve(int grid[][],int row,int col , int dp[][]){
+        if(row > grid.length-1 || col > grid[0].length-1){
             return Integer.MAX_VALUE;
-        if (m == 0 && n == 0)
-            return grid[m][n];
-        if(dp[m][n]!=-1) return dp[m][n];
-        int up = finding(m - 1, n, grid,dp);
-        int left = finding(m, n - 1, grid,dp);
-        return dp[m][n]=(grid[m][n] + Math.min(up, left));
+        }
+        if(row==grid.length-1 && col==grid[0].length-1){
+            return grid[row][col];
+        }
+        if(dp[row][col]!=-1) return dp[row][col];
+
+        int down=solve(grid,row+1,col,dp);
+        int right=solve(grid,row,col+1,dp);
+        return dp[row][col]=grid[row][col]+Math.min(down,right);
     }
 }
