d22ead24ee2a19185df1da101a067266b149b13a
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-08-09 16:37:39 +0530

    Time: 19 ms (16.87%) | Memory: 68.7 MB (19.64%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,34 +1,34 @@
+import java.util.*;
+
 class DisjointSet {
-    List<Integer> size = new ArrayList<>();
     List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
 
     public DisjointSet(int n) {
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             parent.add(i);
             size.add(1);
         }
     }
 
     public int findUltimateParent(int node) {
-        if (parent.get(node) == node) {
-            return node;
-        }
+        if (node == parent.get(node)) return node;
         int ultimateParent = findUltimateParent(parent.get(node));
         parent.set(node, ultimateParent);
         return ultimateParent;
     }
 
     public void unionBySize(int u, int v) {
-        int upU = findUltimateParent(u);
-        int upV = findUltimateParent(v);
-        if (upU == upV) return;
+        int ultimateParentU = findUltimateParent(u);
+        int ultimateParentV = findUltimateParent(v);
+        if (ultimateParentU == ultimateParentV) return;
 
-        if (size.get(upU) > size.get(upV)) {
-            parent.set(upV, upU);
-            size.set(upU, size.get(upU) + size.get(upV));
+        if (size.get(ultimateParentU) > size.get(ultimateParentV)) {
+            parent.set(ultimateParentV, ultimateParentU);
+            size.set(ultimateParentU, size.get(ultimateParentU) + size.get(ultimateParentV));
         } else {
-            parent.set(upU, upV);
-            size.set(upV, size.get(upV) + size.get(upU));
+            parent.set(ultimateParentU, ultimateParentV);
+            size.set(ultimateParentV, size.get(ultimateParentV) + size.get(ultimateParentU));
         }
     }
 }
@@ -39,12 +39,11 @@ class Solution {
 
         DisjointSet ds = new DisjointSet(n);
         int extraEdges = 0;
-
-        for (int[] conn : connections) {
-            if (ds.findUltimateParent(conn[0]) == ds.findUltimateParent(conn[1])) {
+        for (int[] arr : connections) {
+            if (ds.findUltimateParent(arr[0]) == ds.findUltimateParent(arr[1])) {
                 extraEdges++;
             } else {
-                ds.unionBySize(conn[0], conn[1]);
+                ds.unionBySize(arr[0], arr[1]);
             }
         }
 
@@ -54,7 +53,6 @@ class Solution {
                 components++;
             }
         }
-
         return (extraEdges >= components - 1) ? components - 1 : -1;
     }
 }
