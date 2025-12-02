c16d7b53a9272700cffcfa27484085a16477a405
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-12-02 21:25:53 +0530

    Time: 17 ms (40.21%) | Memory: 45.7 MB (9.41%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,30 +1,34 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+        int closestSum= nums[0] + nums[1] + nums[2];
+        int closestDifference=Integer.MAX_VALUE;
         Arrays.sort(nums);
-        int closestSum = nums[0] + nums[1] + nums[2]; // initialize to first triplet
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            int start = i + 1;
-            int end = nums.length - 1;
-
-            while (start < end) {
-                int currentSum = nums[i] + nums[start] + nums[end];
-
-                // update closest sum
-                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
-                    closestSum = currentSum;
+        for(int i=0;i<nums.length;i++){
+            int start=i+1;
+            int end=nums.length-1;
+            while(start<end){
+                if(nums[i]+nums[start]+nums[end]==target){
+                    closestDifference = 0;
+                    closestSum = target;
+                    return closestSum;
                 }
-
-                if (currentSum == target) {
-                    return target;  // best possible
-                } else if (currentSum < target) {
-                    start++;
-                } else {
+                else if(nums[i]+nums[start]+nums[end] > target){
+                    if(closestDifference > Math.abs((nums[i]+nums[start]+nums[end])-target)){
+                        closestDifference = Math.abs((nums[i]+nums[start]+nums[end])-target);
+                        closestSum = nums[i]+nums[start]+nums[end];
+                    }
                     end--;
                 }
+                else{
+                    if(closestDifference > Math.abs((nums[i]+nums[start]+nums[end])-target)){
+                        closestDifference = Math.abs((nums[i]+nums[start]+nums[end])-target);
+                        closestSum = nums[i]+nums[start]+nums[end];
+                    }
+                    start++;
+                }
+
             }
         }
-
         return closestSum;
     }
 }
