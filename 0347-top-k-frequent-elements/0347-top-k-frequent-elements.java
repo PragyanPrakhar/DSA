06c3481839353cf992c110ad7b552f6a7f0e40cc
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2025-12-10 23:52:01 +0530

    Time: 12 ms (84.04%), Space: 47.4 MB (85.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,15 +1,19 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int num:nums) hm.put(num,hm.getOrDefault(num,0)+1);
+        for(int num:nums){
+            hm.put(num,hm.getOrDefault(num,0)+1);
+        }
+
         List<Integer>[] bucket=new List[nums.length+1];
-        for(int num:hm.keySet()){
-            int freq=hm.get(num);
+        for(int i:hm.keySet()){
+            int freq=hm.get(i);
             if(bucket[freq]==null){
                 bucket[freq]=new ArrayList<>();
             }
-            bucket[freq].add(num);
+            bucket[freq].add(i);
         }
+        // int ans[]=new int[nums.length];
         ArrayList<Integer> ans=new ArrayList<>();
         for(int i=bucket.length-1;i>=0;i--){
             if(bucket[i]!=null){
@@ -17,10 +21,11 @@ class Solution {
             }
         }
         int res[]=new int[k];
-
-        for(int i=0;i<k; i++){
+        for(int i=0;i<k;i++){
             res[i]=ans.get(i);
         }
         return res;
+
+        
     }
 }
