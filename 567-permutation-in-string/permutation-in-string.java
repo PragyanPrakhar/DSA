8cc64ea7c70e322aba1527306c2b248f250643a6
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2026-01-06 00:01:46 +0530

    Time: 6 ms (86.54%) | Memory: 44.2 MB (43.27%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,25 +1,31 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length()>s2.length()) return false;
-        int[] freqS1=new int[26];
-        int[] freqS2=new int[26];
-        for(char ch:s1.toCharArray()){
+        if(s1.length() > s2.length()) return false;
+        // HashMap<Character,Integer> hm=new HashMap<>();
+        int freqS1[]=new int[26];
+        int freqS2[]=new int[26];
+
+        for (char ch:s1.toCharArray()){
             freqS1[ch-'a']++;
         }
+
         for(int i=0;i<s1.length();i++){
             freqS2[s2.charAt(i)-'a']++;
         }
+
         for(int i=s1.length();i<s2.length();i++){
-            if(isEqual(freqS1,freqS2)) return true; 
+            if(isEqual(freqS1,freqS2)) return true;
             freqS2[s2.charAt(i)-'a']++;
             freqS2[s2.charAt(i-s1.length())-'a']--;
         }
         return isEqual(freqS1,freqS2);
     }
-    private boolean isEqual(int[] freqS1,int[] freqS2){
+
+    private boolean isEqual(int freqS1[],int freqS2[]){
         for(int i=0;i<26;i++){
             if(freqS1[i]!=freqS2[i]) return false;
         }
         return true;
     }
+    
 }
