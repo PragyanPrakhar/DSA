822ef409044b3e2e4c3efd06c4159dfc5f685393
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2026-01-09 00:26:16 +0530

    Time: 5 ms (83.4%), Space: 47.2 MB (14.02%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,59 +1,38 @@
+import java.util.Stack;
+
 class MinStack {
-    Stack<Integer> st;
-    Stack<Integer> trackMin;
+
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
     public MinStack() {
-        st=new Stack<>();
-        trackMin=new Stack<>();
+        stack = new Stack<>();
+        minStack = new Stack<>();
     }
     
     public void push(int val) {
-        st.push(val);
-        ArrayList<Integer> arr=new ArrayList<>();
-        int min=-1;
-        if(!trackMin.isEmpty()){
-            while(!trackMin.isEmpty() && trackMin.peek()<val){
-                arr.add(trackMin.pop());
-            }
-        }
-        Collections.sort(arr);
-        trackMin.push(val);
-        for(int i=arr.size()-1;i>=0;i--){
-            trackMin.push(arr.get(i));
+        stack.push(val);
+
+        // Push minimum so far
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
         }
     }
     
     public void pop() {
-        int poppedValue=st.pop();
-        ArrayList<Integer> arr=new ArrayList<>();
-        while(trackMin.peek()!=poppedValue && !trackMin.isEmpty()){
-            arr.add(trackMin.pop());
-        }
-        trackMin.pop();
-        while(!trackMin.isEmpty()){
-            arr.add(trackMin.pop());
-        }
-        Collections.sort(arr);
-        for(int i=arr.size()-1;i>=0;i--){
-            trackMin.push(arr.get(i));
-        }
+        int removed = stack.pop();
 
-
+        // If popped element is current minimum
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
     }
     
     public int top() {
-        return st.peek();       
+        return stack.peek();
     }
     
     public int getMin() {
-        return trackMin.peek();
+        return minStack.peek();
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
