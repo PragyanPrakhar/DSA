d70eb73e6cd8118143b68f8541ef5647f539ec8c
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2026-01-19 13:49:48 +0530

    Time: 109 ms (13.22%) | Memory: 105.9 MB (25.21%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,15 +1,15 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int pair[][]=new int[position.length][2];
+        int[][] posSpeed=new int[position.length][2];
         for(int i=0;i<position.length;i++){
-            pair[i][0]=position[i];
-            pair[i][1]=speed[i];
+            posSpeed[i][0]=position[i];
+            posSpeed[i][1]=speed[i];
         }
-        //sorting the stack on the basis of position in descending order
-        Arrays.sort(pair,(a,b)->Integer.compare(b[0],a[0]));
+        Arrays.sort(posSpeed,(a,b)->Integer.compare(b[0],a[0]));
+
         Stack<Double> stack=new Stack<>();
         // the idea to solve this question is whenever the time taken to reach the target of a car which is behind the some other car is less than that , then they will definitely form the fleet .
-        for(int[] p:pair){
+        for(int[] p:posSpeed){
             stack.push((double)(target-p[0])/p[1]);
             if(stack.size()>=2){
                 if(stack.peek()<=stack.get(stack.size()-2)){
@@ -18,6 +18,5 @@ class Solution {
             }
         }
         return stack.size();
-
     }
 }
