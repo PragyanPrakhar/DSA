64b90e65be5e89650e704d8a498f01061d7cf4d3
Pragyan Prakhar <92442393+PragyanPrakhar@users.noreply.github.com>
2026-01-19 13:59:05 +0530

    Time: 92 ms (30.18%) | Memory: 95.6 MB (78.88%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,22 +1,24 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int[][] posSpeed=new int[position.length][2];
-        for(int i=0;i<position.length;i++){
-            posSpeed[i][0]=position[i];
-            posSpeed[i][1]=speed[i];
-        }
-        Arrays.sort(posSpeed,(a,b)->Integer.compare(b[0],a[0]));
+        int n = position.length;
+        double[][] cars = new double[n][2];
 
-        Stack<Double> stack=new Stack<>();
-        // the idea to solve this question is whenever the time taken to reach the target of a car which is behind the some other car is less than that , then they will definitely form the fleet .
-        for(int[] p:posSpeed){
-            stack.push((double)(target-p[0])/p[1]);
-            if(stack.size()>=2){
-                if(stack.peek()<=stack.get(stack.size()-2)){
-                    stack.pop();
-                }
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = (double)(target - position[i]) / speed[i];
+        }
+
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+
+        int fleets = 0;
+        double maxTime = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (cars[i][1] > maxTime) {
+                fleets++;
+                maxTime = cars[i][1];
             }
         }
-        return stack.size();
+        return fleets;
     }
 }
